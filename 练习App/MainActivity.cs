2831de71471214389a481db6cf77ea0833f7dc9a
ly <1492620108@qq.com>
2019-01-27 11:32:30 +0800

    first practive for Xamarin.Android

---
+++ 练习App/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using AlertDialog = Android.App.AlertDialog;
+
+namespace 练习App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.editText1);
+            Button translateButton = (Button)FindViewById(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+            callButton.Enabled = false;
+            string translateNumber = string.Empty;
+            translateButton.Click += (s, e) => {
+                translateNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrEmpty(translateNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call" + translateNumber;
+                    callButton.Enabled = true;
+                }
+            };
+            callButton.Click += (s, e) => 
+            {
+                //对话框
+                var callDialog = new AlertDialog.Builder(this);
+
+                //对话框内容
+                callDialog.SetMessage("Call" + translateNumber + "?");
+
+
+                
+                //拨打按钮
+                callDialog.SetNegativeButton("Call", delegate          //SetPositiveButton
+                {
+                    //使用意图拨打电话
+                    var callIntent = new Intent(Intent.ActionCall);
+
+                    //将需要拨打的电话设置为意图的参数
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translateNumber));
+
+                    StartActivity(callIntent);
+                });
+                //取消按钮
+                callDialog.SetNeutralButton("Cancel", delegate { });
+                callDialog.Show();
+
+            };
+        }
+    }
+}
---
+++ 练习App/PhoneTranslator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace 练习App
+{
+    public class PhoneTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return "";
+            else
+                raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        public static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+                return 2;
+            else if ("DEF".Contains(c))
+                return 3;
+            else if ("GHI".Contains(c))
+                return 4;
+            else if ("JKL".Contains(c))
+                return 5;
+            else if ("MNO".Contains(c))
+                return 6;
+            else if ("PQRS".Contains(c))
+                return 7;
+            else if ("TUV".Contains(c))
+                return 8;
+            else if ("WXYZ".Contains(c))
+                return 9;
+            return null;
+        }
+    }
+}
